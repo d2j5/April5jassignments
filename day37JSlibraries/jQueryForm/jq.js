77e5d1ee108706787fc37b2f5460fd3f975e668b
Javier <d2j5@hotmail.com>
2023-04-25 22:41:56 -0400

    v2

--- day37JSlibraries/jQueryForm/jq.js
+++ day37JSlibraries/jQueryForm/jq.js
@@ -1,3 +1,30 @@
-$(document).ready(function() {
-          
+
+$(function() {
+    $("#submit").on("click", function(e) {
+event.preventDefault();
+
+    let name = $('#name').val();      //$('') = document.getElementById('')'
+    console.log(name);
+    let email = $('#email').val();
+    let phone = $('#phone').val();
+    
+    let required = [name, email, phone]
+
+    for (let i = 0; i < required.length; i++) {
+        if (required[i] === '') {
+            $("#message").text("Please Fill Out Required Fields");
+            $("#message").addClass("warning");
+            $("input").eq(i).on("change", function() {
+            $("label").eq(i).removeClass("warning");
+             })
+        } else {
+            $("#message").text("");
+            $("#message").removeClass("warning");
+            $("label").eq(i).removeClass("warning");
+        } 
+        }  if (!$("label").hasClass("warning")) {
+    $('#form').remove();
+    $("h2").html('Thanks for your feedback!');
+}
+})
 });
