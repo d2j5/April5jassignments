dfd2addcbc8a442330e338581c551b3dbb81842e
Javier <d2j5@hotmail.com>
2023-04-26 16:24:10 -0400

    v1

---
+++ day38react/reactApp/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './counter.js';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
